a86066d8c6a4de3cb5b07be649c09ceeab02d7d3
nguyenductoan <nguyenductoann@gmail.com>
2019-09-05 16:58:22 +0700

    fix

--- src/TapRating.js
+++ src/TapRating.js
@@ -33,24 +33,24 @@ export default class TapRating extends Component {
     this.setState({ position: defaultRating })
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.defaultRating !== this.props.defaultRating) {
-      this.setState({ position: nextProps.defaultRating })
-    }
+  // componentWillReceiveProps(nextProps) {
+  //   if (nextProps.defaultRating !== this.props.defaultRating) {
+  //     this.setState({ position: nextProps.defaultRating })
+  //   }
+  // }
+
+  static getDerivedStateFromProps(nextProps, prevState){
+    if (nextProps.defaultRating !== prevState.defaultRating){
+      return { defaultRating: nextProps.defaultRating };
+   }
+   else return null;
+ }
+
+ componentDidUpdate(prevProps, prevState) {
+  if (prevState.defaultRating !== this.state.defaultRating) {
+    this.setState({ position: this.state.defaultRating })
   }
-
-//   static getDerivedStateFromProps(nextProps, prevState){
-//     if (nextProps.defaultRating !== prevState.defaultRating){
-//       return { defaultRating: nextProps.defaultRating };
-//    }
-//    else return null;
-//  }
-
-//  componentDidUpdate(prevProps, prevState) {
-//   if (prevState.defaultRating !== this.state.defaultRating) {
-//     this.setState({ position: this.state.defaultRating })
-//   }
-// }
+}
 
   renderStars(rating_array) {
     return _.map(rating_array, (star, index) => {
